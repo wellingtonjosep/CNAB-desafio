c11e3c5dd97d5af20609209f05a42fded083af84
Wellington José <paivawellington23@gmail.com>
2022-10-10 16:15:38 -0300

    fix: fixed views data

--- documents/serializers.py
+++ documents/serializers.py
@@ -7,5 +7,22 @@ class DocumentSerializer(serializers.ModelSerializer):
         fields = ["id","type","date","value","cpf","card","hour","store_owner","store_name"]
     
     def create(self, validated_data):
-        print(validated_data)
+        if (validated_data["type"] == "1"):
+            validated_data["type"] = "Débito"
+        if (validated_data["type"] == "2"):
+            validated_data["type"] = "Boleto"
+        if (validated_data["type"] == "3"):
+            validated_data["type"] = "Financiamento"
+        if (validated_data["type"] == "4"):
+            validated_data["type"] = "Crédito"
+        if (validated_data["type"] == "5"):
+            validated_data["type"] = "Recebimento Empréstimo"
+        if (validated_data["type"] == "6"):
+            validated_data["type"] = "Vendas"
+        if (validated_data["type"] == "7"):
+            validated_data["type"] = "Recebimento TED"
+        if (validated_data["type"] == "8"):
+            validated_data["type"] = "Recebimento DOC"
+        if (validated_data["type"] == "9"):
+            validated_data["type"] = "Aluguel"
         return Document.objects.create(**validated_data)
--- documents/views.py
+++ documents/views.py
@@ -9,13 +9,13 @@ class FileUploadView(APIView):
 
     def post(self, request, filename, format=None):
         file = request.FILES["file"].read().decode().split("\n")
+        if not (file):
+            return Response(data={"error": "File not found"},status=400)
         data = []
         for item in file:  
             serializer = DocumentSerializer( data={"type": item[0],"date": item[1:9], "value": item[9:19],"cpf": item[19:30], "card": item[30:42], "hour": item[42:48], "store_owner": item[48:62], "store_name": item[62:81],})
             serializer.is_valid(raise_exception=True)
             serializer.save()
-            data = [serializer.data] + data
+            data.append(serializer.data)
 
         return Response(data=data,status=201)
-
-
